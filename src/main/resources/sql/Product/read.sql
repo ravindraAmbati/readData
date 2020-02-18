659bbaa6c12bb8d8fbe9a34898f7c9cabfa96a3b
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-02-18 22:44:11 +0530

    updated read.sql

--- src/main/resources/sql/Product/read.sql
+++ src/main/resources/sql/Product/read.sql
@@ -13,3 +13,7 @@ select * from product where name IS NOT NULL order by price desc limit 50;
 select * from product where name IS NOT NULL order by price asc limit 50;
 select * from product where name IS NOT NULL order by price desc limit 100;
 select * from product where name IS NOT NULL order by price asc limit 100;
+select p1.name, p1.quantity, p1.price, p1.timeStamp, p2.name, p2.quantity, p2.price, p2.timeStamp
+from product p1, product p2
+where p1.id != p2.id and p1.prodid = p2.prodid and 
+p1.price != p2.price and p1.name is not NULL and p2.name is not NULL order by p1.prodid;
