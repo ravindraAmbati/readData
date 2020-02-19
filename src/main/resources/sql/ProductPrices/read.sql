bbec4eeb80ec69e72040e43e25964c6de8df7e48
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-02-20 00:22:43 +0530

    updated read.sql

--- src/main/resources/sql/ProductPrices/read.sql
+++ src/main/resources/sql/ProductPrices/read.sql
@@ -2,3 +2,4 @@ select MAX(PRICE) from Product where prodId='10000222';
 select * from Product where prodId='10000100';
 select * from ProductPrices;
 select * from Product where price = 0;
+select ((maxprice-minprice)/price)*100,* from ProductPrices where stddevprice != 0;
