341d78dea10d79d84529654a3046663380607111
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-03-07 19:57:43 +0530

    updated read.sql

--- src/main/resources/sql/ProductPrices/read.sql
+++ src/main/resources/sql/ProductPrices/read.sql
@@ -3,3 +3,10 @@ select * from Product where prodId='10000100';
 select * from ProductPrices;
 select * from Product where price = 0;
 select ((maxprice-minprice)/price)*100,* from ProductPrices where stddevprice != 0;
+select 
+((maxprice-minprice)/prices.price)*100 pert,* 
+from ProductPrices prices, Product prod
+where prod.timestamp <= '2020-03-08'  and prod.timestamp >= '2020-03-07' 
+and stddevprice != 0 and prices.prodid  =  prod.prodid 
+and prices.timestamp <= '2020-03-08'  and prices.timestamp >= '2020-03-07' 
+order by pert desc
